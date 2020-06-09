888435383924a577c2067de309ddbd06c1e3f250
simbato <simbato@student.math.hr>
2020-06-09 16:18:59 -0700

    dodani has_registered i reg_sequence

--- model/kladionicaservice.class.php
+++ model/kladionicaservice.class.php
@@ -23,7 +23,7 @@ class KladionicaService
 		if( $row === false )
 			return null;
 		else
-			return new User( $row['id'], $row['username'], $row['password_hash'], $row['email'], $row['iznos']);
+			return new User( $row['id'], $row['username'], $row['password_hash'], $row['email'], $row['iznos'], $row['has_registred'], $row['registration_sequence']);
 	}
 	function getUserByUsername( $username )
 	{
@@ -40,7 +40,7 @@ class KladionicaService
 		if( $row === false )
 			return null;
 		else
-			return new User( $row['id'], $row['username'], $row['password_hash'], $row['email'],$row['iznos']);
+			return new User( $row['id'], $row['username'], $row['password_hash'], $row['email'], $row['iznos'], $row['has_registred'], $row['registration_sequence']);
 	}
 
 	function getAllUsers( )
@@ -56,7 +56,7 @@ class KladionicaService
 		$arr = array();
 		while( $row = $st->fetch() )
 		{
-			$arr[] = new User( $row['id'], $row['username'], $row['password_hash'], $row['email'], $row['iznos']);
+			$arr[] = new User( $row['id'], $row['username'], $row['password_hash'], $row['email'], $row['iznos'], $row['has_registred'], $row['registration_sequence']);
 		}
 
 		return $arr;
--- model/user.class.php
+++ model/user.class.php
@@ -2,14 +2,17 @@
 
 class User
 {
-	protected $id, $username, $password, $email;
+	protected $id, $username, $password, $email, $iznos, $has_registered, $registration_sequence;
 
-	function __construct( $id, $username, $password, $email)
+	function __construct( $id, $username, $password, $email, $iznos, $has_registered, $registration_sequence)
 	{
 		$this->id = $id;
 		$this->username = $username;
 		$this->email = $email;
 		$this->password = $password;
+		$this->iznos = $iznos;
+		$this->has_registered = $has_registered;
+		$this->registration_sequence = $registration_sequence;
 	}
 
 	function __get( $prop ) { return $this->$prop; }
