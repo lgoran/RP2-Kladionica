384b58132f5e70e68b7f9ce239295f4ed25fcdd3
gorlaligit <gorlali@student.math.hr>
2020-06-10 15:49:41 +0200

    paypal uplata

--- view/vasi_listici.php
+++ view/vasi_listici.php
@@ -1,4 +1,19 @@
 <?php require_once __DIR__ . '/_header.php'; ?>
+Uplati na račun:
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="EM5PK9F6W3TVJ">
+<table>
+<tr><td><input type="hidden" name="on0" value="Koliko želite kredita?">Koliko želite kredita?</td></tr><tr><td><select name="os0">
+	<option value="100 kredita">100 kredita €10.00 EUR</option>
+	<option value="1500 kredita">1500 kredita €100.00 EUR</option>
+	<option value="20000 kredita">20000 kredita €1,000.00 EUR</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="EUR">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
 
 
 <?php
